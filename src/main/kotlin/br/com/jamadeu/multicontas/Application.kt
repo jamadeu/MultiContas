0c1c9e59017d712aee0d34ae352505db9f333a16
Jean Amadeu <jean.amadeu2@gmail.com>
2022-05-19 06:47:54 -0300

    Start project

---
+++ src/main/kotlin/br/com/jamadeu/multicontas/Application.kt
@@ -0,0 +1,11 @@
+package br.com.jamadeu.multicontas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
---
+++ src/test/kotlin/br/com/jamadeu/multicontas/MulticontasApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.jamadeu.multicontas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MulticontasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
